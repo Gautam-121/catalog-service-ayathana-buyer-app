526f37fb262ef98b8f89bb41ae8d94ca65b36b8a
Preeti684-bhardwaj <bhardwajpreeti684@gmail.com>
2024-09-30 18:46:34 +0530

    update iso_time_utils

--- utils/iso_time_utils.py
+++ utils/iso_time_utils.py
@@ -9,7 +9,7 @@ def calculate_duration_in_seconds(iso8601dur: str):
         duration = isodate.parse_duration(iso8601dur)
         return duration.total_seconds()
     except ISO8601Error:
-        log_error(f"Got error while parsing iso8601 string {iso8601dur}")
+        log_error(f"Got errors while parsing iso8601 string {iso8601dur}")
         return 0
 
 
